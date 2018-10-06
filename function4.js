68c1d947f9fb5df9db2912cafb36ec059b69da70
SonyaSytnik <sofia.sytnik2012@gmail.com>
2018-10-06 14:12:03 +0300

    Add simple functions

---
+++ function4.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const power = Math.pow;
+const square = a => power(a, 2);
+const cube = a => power(a, 3);
+
+console.log(power(2, 2));
+console.log(square(2));
+console.log(cube(2));
