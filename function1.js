7ce13a4b035279fb22033bc55ee76d56b9c7297e
SonyaSytnik <sofia.sytnik2012@gmail.com>
2018-10-06 13:18:46 +0300

    Function with cities

--- function1.js
+++ function1.js
@@ -1,1 +1,31 @@
-Project 1
+'use strict'
+
+const cities = ['Kiev', 'Zhytomyr', 'Odessa', 'Kharkiv', 'Lviv'];
+const func = s => s.length;
+
+function func1() {
+  const cities = ['Kiev', 'Zhytomyr'];
+  const func = s => s.toUpperCase();
+
+  console.dir({ cities });
+  console.dir(cities.map(func));
+
+  {
+    const func = s => s.toLowerCase();
+
+    console.dir({ cities });
+    console.dir(cities.map(func));
+  }
+
+  {
+    const cities = ['Odessa', 'Kharkiv', 'Lviv'];
+
+    console.dir({ cities });
+    console.dir(cities.map(func));
+  }
+}
+
+func1();
+
+console.dir({ cities });
+console.dir(cities.map(func));
